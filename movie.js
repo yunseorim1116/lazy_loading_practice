440a12b0a2772fbd754b3e071bbcf9df58f8e786
seorim <hhyjs2r@gmail.com>
2023-04-08 20:15:15 +0900

    [feature] lazy loading test code

--- movie.js
+++ movie.js
@@ -2,13 +2,10 @@
 import { API_KEY } from "./apikey.js";
 
 const fetchMovieData = async(movieDate) =>{
-    console.log(API_KEY )
-
   const movieApiUrl = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json';
 
   try {
     const response = await axios.get(`${movieApiUrl}?key=${API_KEY}&targetDt=${movieDate}`);
-    console.log(response)
     const resMovieDataList = response.data.movieListResult.movieList;
     return resMovieDataList;
   } catch (error) {
@@ -25,6 +22,41 @@ const createMockData = (movieData)=>{
   return newMovieList
 }
 
+  const handleLazyLoad = () => {
+    console.log('b')
+    const images = document.querySelectorAll('.movie-img');
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    const loadImage = image => {
+      const src = image.getAttribute('data-src');
+      const img = new Image();
+      img.src = src;
+      console.log(img)
+      img.onload = () => {
+        image.setAttribute('src', src);
+        image.classList.remove('lazy');
+      };
+    };
+
+    const handleIntersection = entries => {
+      console.log('bs')
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const image = entry.target;
+          loadImage(image);
+          observer.unobserve(image);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+    images.forEach(image => observer.observe(image));
+  };
+
 const renderMovieData = (processedData) => {
   const movieContainer = document.createElement('div');
   movieContainer.classList.add('movie-container');
@@ -36,7 +68,10 @@ const renderMovieData = (processedData) => {
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
-    movieImg.src = "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG";
+    const movieImgSrc = "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG";
+    movieImg.setAttribute('data-src', movieImgSrc);
+    movieImg.setAttribute('alt', movieData.movieNmEn);
+    // movieImg.src = "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/32E9/image/BA2Qyx3O2oTyEOsXe2ZtE8cRqGk.JPG";
     movieElement.appendChild(movieImg);
 
     const br = document.createElement("br");
@@ -53,7 +88,6 @@ const renderMovieData = (processedData) => {
     movieElement.appendChild(movieContent);
 
     // 생성된 요소를 movieContainer에 추가
-
     movieContainer.appendChild(movieElement);
   }
 
@@ -61,12 +95,16 @@ const renderMovieData = (processedData) => {
   document.body.appendChild(movieContainer);
 };
 
-(async function() {
+
+
+
+window.onload = async function() {
   try {
     const movieData = await fetchMovieData('20220407');
     const processedData = createMockData(movieData);
     renderMovieData(processedData);
+    handleLazyLoad();
   } catch (error) {
     console.log(error);
   }
-})();
+};
